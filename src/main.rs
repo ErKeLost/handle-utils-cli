2a371bc7be65c6a15db7cb86fc6e62dc9e757b4c
erkelost <1256029807@qq.com>
2024-06-21 15:57:11 +0800

    chore: init cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "adny", version, about)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+
+#[derive(Debug, Parser)]
+enum  SubCommand {
+    #[command(name = "csv", about = "show CSV transformation")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long)]
+    input: String,
+    // default_value 实现了 FromStr 的 trait
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(short, long, default_value_t = true)]
+    header: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+    println!("{:?}", opts)
 }
